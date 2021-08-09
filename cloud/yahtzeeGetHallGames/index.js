88eeaded90b912284950541d12ba58247a759275
任无名 <renwuming@foxmail.com>
2021-08-09 15:51:48 +0800

    feat: 🎸 大厅云函数优化

--- cloud/yahtzeeGetHallGames/index.js
+++ cloud/yahtzeeGetHallGames/index.js
@@ -10,17 +10,32 @@ exports.main = async (event) => {
   const db = cloud.database();
   const _ = db.command;
 
-  // 查找所有【最近3小时】【未结束的】游戏
+  // 查找所有符合条件的游戏
   const TREE_HOURS = 3 * 60 * 60 * 1000;
   const TIME = new Date(Date.now() - TREE_HOURS);
   const _skip = +(skip || 0);
   const _pageLength = +(pageLength || 10);
   const list = await db
     .collection("yahtzee_games")
-    .where({
-      _updateTime: _.gt(TIME),
-      end: _.neq(true),
-    })
+    .where(
+      _.and([
+        // 最近3小时的
+        {
+          _updateTime: _.gt(TIME),
+        },
+        _.or([
+          // 未开始的
+          {
+            start: _.neq(true),
+          },
+          // 或已经开始的、未结束的、多人游戏
+          {
+            end: _.neq(true),
+            "players.1": _.exists(1),
+          },
+        ]),
+      ])
+    )
     .orderBy("_updateTime", "desc")
     .skip(_skip)
     .limit(_pageLength)

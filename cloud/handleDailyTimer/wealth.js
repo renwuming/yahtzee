708c116e4493d6d550c7d9dcd9f927e2e38bd493
renwuming <renwuming@foxmail.com>
2022-03-04 11:19:41 +0800

    feat: 🎸 优化云函数

--- cloud/handleDailyTimer/wealth.js
+++ cloud/handleDailyTimer/wealth.js
@@ -1,12 +1,10 @@
 async function execHandleDailyWealthRecord(db) {
   const _ = db.command;
 
-  const THOUSAND_LEN = 10 * 10;
-  const THOUSAND = 1000;
-
-  for (let i = 0; i < THOUSAND_LEN; i++) {
-    const skip = i * THOUSAND;
-
+  const GROUP_LEN = 2 * 100;
+  const GROUP = 300;
+  for (let i = 0; i < GROUP_LEN; i++) {
+    const skip = i * GROUP;
     // 清空每日福利的领取记录
     const list = await db
       .collection("players")
@@ -14,25 +12,38 @@ async function execHandleDailyWealthRecord(db) {
         wealthRecord: _.exists(1),
       })
       .skip(skip)
-      .limit(THOUSAND)
+      .limit(GROUP)
       .get()
       .then((res) => res.data);
-    for (let j = 0; j < list.length; j++) {
-      const { _id, wealthRecord } = list[j];
-      const updateData = {};
-      for (let key in wealthRecord) {
-        const { refresh } = wealthRecord[key];
-        if (refresh === "day") {
-          updateData[`wealthRecord.${key}.times`] = 0;
+    await Promise.all(
+      list.map((item) => {
+        const { _id, wealthRecord } = item;
+        const updateData = {};
+        for (let key in wealthRecord) {
+          const { refresh } = wealthRecord[key];
+          if (refresh === "day") {
+            updateData[`wealthRecord.${key}.times`] = 0;
+          }
         }
-      }
-      if (Object.keys(updateData).length > 0) {
-        db.collection("players").doc(_id).update({
-          data: updateData,
-        });
-      }
-    }
+        if (Object.keys(updateData).length > 0) {
+          return db
+            .collection("players")
+            .doc(_id)
+            .update({
+              data: updateData,
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+        return null;
+      })
+    );
   }
+}
+
+function sleep(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
 }
 
 module.exports = {

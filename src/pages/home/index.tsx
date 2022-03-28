7028a9214f4bff67be1300f61d8629556a608937
任无名 <renwuming@renwumingdeMacBook-Pro-2021.local>
2022-03-28 16:21:05 +0800

    feat: 🎸 解决违规问题，发布公告

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,19 @@
-import Taro, { useShareAppMessage } from "@tarojs/taro";
+import Taro, { useDidShow, useShareAppMessage } from "@tarojs/taro";
+import { useState } from "react";
 import { View, Image, Text } from "@tarojs/components";
-import { AtButton, AtIcon } from "taro-ui";
+import {
+  AtButton,
+  AtIcon,
+  AtModal,
+  AtModalContent,
+  AtModalHeader,
+} from "taro-ui";
 import { getUserProfile, navigateTo, VERSION } from "@/utils";
 import MyPlayer from "../../Components/MyPlayer";
 // @ts-ignore
 import RankIcon from "../../assets/imgs/rank.png";
 // @ts-ignore
 import WechatIcon from "../../assets/imgs/wechat.png";
-// @ts-ignore
-// import RewardIcon from "../../assets/imgs/reward.png";
-// import { createGame } from "../Rummy/game/api";
 import "./index.scss";
 
 export default function Index() {
@@ -23,42 +27,58 @@ export default function Index() {
     };
   });
 
+  const [showModal, setShowModal] = useState<boolean>(true);
+
+  useDidShow(() => {
+    setShowModal(true);
+  });
+
   const gameList = [
     {
-      name: "拉密牌",
+      name: "拉密数字棋",
       imgUrl:
         "https://cdn.renwuming.cn/static/diceGames/imgs/covers/rummy-cover.jpg",
-      pageType: "Rummy",
-      pagePath: `hall/index`,
+      navigateFn() {
+        Taro.navigateToMiniProgram({
+          appId: "wx12825b7bafc3db6e",
+        });
+      },
     },
-    {
-      name: "欲罢不能",
-      imgUrl:
-        "https://cdn.renwuming.cn/static/diceGames/imgs/covers/cantstop-cover.jpg",
-      pageType: "CantStop",
-      pagePath: `hall/index`,
-    },
-    {
-      name: "神奇形色牌",
-      imgUrl:
-        "https://cdn.renwuming.cn/static/diceGames/imgs/covers/set-cover.jpg",
-      pageType: "Set",
-      pagePath: `hall/index`,
-    },
-    {
-      name: "快艇骰子",
-      imgUrl:
-        "https://cdn.renwuming.cn/static/diceGames/imgs/covers/yahtzee-cover.jpg",
-      pageType: "Yahtzee",
-      pagePath: `hall/index`,
-    },
-    {
-      name: "火星骰",
-      imgUrl:
-        "https://cdn.renwuming.cn/static/diceGames/imgs/covers/martian-cover.jpg",
-      pageType: "Martian",
-      pagePath: `hall/index`,
-    },
+    // {
+    //   name: "拉密牌",
+    //   imgUrl:
+    //     "https://cdn.renwuming.cn/static/diceGames/imgs/covers/rummy-cover.jpg",
+    //   pageType: "Rummy",
+    //   pagePath: `hall/index`,
+    // },
+    // {
+    //   name: "欲罢不能",
+    //   imgUrl:
+    //     "https://cdn.renwuming.cn/static/diceGames/imgs/covers/cantstop-cover.jpg",
+    //   pageType: "CantStop",
+    //   pagePath: `hall/index`,
+    // },
+    // {
+    //   name: "神奇形色牌",
+    //   imgUrl:
+    //     "https://cdn.renwuming.cn/static/diceGames/imgs/covers/set-cover.jpg",
+    //   pageType: "Set",
+    //   pagePath: `hall/index`,
+    // },
+    // {
+    //   name: "快艇骰子",
+    //   imgUrl:
+    //     "https://cdn.renwuming.cn/static/diceGames/imgs/covers/yahtzee-cover.jpg",
+    //   pageType: "Yahtzee",
+    //   pagePath: `hall/index`,
+    // },
+    // {
+    //   name: "火星骰",
+    //   imgUrl:
+    //     "https://cdn.renwuming.cn/static/diceGames/imgs/covers/martian-cover.jpg",
+    //   pageType: "Martian",
+    //   pagePath: `hall/index`,
+    // },
     {
       name: "截码战",
       imgUrl:
@@ -90,7 +110,7 @@ export default function Index() {
         {gameList.map((item) => {
           const { name, imgUrl, pageType, pagePath, navigateFn } = item;
           return (
-            <View className="item-box at-col at-col-4">
+            <View key={null} className="item-box at-col at-col-4">
               <AtButton
                 onClick={() => {
                   getUserProfile(() => {
@@ -112,6 +132,16 @@ export default function Index() {
         })}
       </View>
       <View className="bottom-list at-row at-row__align--center">
+        <AtButton
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          <View className="icon-btn">
+            <AtIcon value="message" size="32" color="#4871b6"></AtIcon>
+            <Text className="text">公告</Text>
+          </View>
+        </AtButton>
         <AtButton
           onClick={() => {
             getUserProfile(() => {
@@ -159,6 +189,27 @@ export default function Index() {
           </View>
         </AtButton> */}
       </View>
+
+      <AtModal
+        isOpened={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+      >
+        <AtModalHeader>
+          <Text>重要通知</Text>
+        </AtModalHeader>
+        <AtModalContent>
+          <View className="notice-box">
+            <Text>由于微信审核的原因，本小程序大部分内容已经停运。</Text>
+            <Text>不要担心，我们会将内容陆续迁移到新的小游戏。</Text>
+            <Text>
+              截止目前，【拉密数字棋】小游戏已经上线了一个单机版本，欢迎大家试玩，完整功能预计在“五一”之前完成。
+            </Text>
+            <Text>最后感谢大家对任同学小程序的支持，欢迎加群交流，谢谢！</Text>
+          </View>
+        </AtModalContent>
+      </AtModal>
     </View>
   );
 }
